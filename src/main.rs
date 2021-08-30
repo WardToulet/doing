cf71a11709566557b37d12c83e0688ec8bda7b38
Ward Toulet <ward@toulet.net>
2021-08-30 23:20:18 +0200

    [feat] Added minimal subcommands
    Used StructOpt to implement the cli interface
    
    Commands
    - `on <item>` set the currently tracked item
    - `done` finsish the currently tracked item and write it to the store

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+mod record_store;
+mod record;
+
+use crate::record::{Current, Record};
+use crate::record_store::RecordStore;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+enum Command {
+    On {
+        item: String,
+    },
+
+    Done,
+
+    What,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    match Command::from_args() {
+        Command::On { item } => { Record::start(&item).save().unwrap(); }
+        Command::Done => { 
+            match Current::open() {
+                Some(current) => {
+                    let record = current.stop();
+                    Current::clear();
+                    
+                    println!("Stop tracking {:#?}", record);
+
+                    let mut record_store = record_store::CsvRecordStore::open();
+                    record_store.push(record);
+                    record_store.save();
+                }
+                None => {
+                    println!("You are currently not tracking anything, to start tracking type `working on <your item>`");
+                },
+            }
+        },
+        Command::What => todo!(),
+    }
 }
